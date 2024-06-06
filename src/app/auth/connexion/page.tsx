cd5f05666f8f79033f5ad6326590fe732bdd6107
Chrisdlm60 <delmotte.christophe@hotmail.fr>
2024-06-06 12:51:00 +0200

    register + connexion

--- src/app/auth/connexion/page.tsx
+++ src/app/auth/connexion/page.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const Page = () => {
+export default function SignIn() {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section className="signin-ctn">
+        <h2>Se connecter</h2>
 
-export default Page
+        <form action="">
+          <label htmlFor="email">Email *</label>
+          <input
+            required
+            placeholder="Votre email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="email">Mot de passe *</label>
+          <input
+            required
+            placeholder="Votre mot de passe"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="signin-btn" type="submit">Connexion</button>
+        </form>
+
+        <p className="signin_to_up">Pas encore de compte ? </p>
+        <p className="signin_to_up link">
+          <Link href="/enregistrement">Vous pouvez en créer un.</Link>
+        </p>
+      </section>
+    </>
+  );
+}
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,11 +1,69 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const Page = () => {
+export default function SignUp() {
+  
+  const [lastname, setLastname] = useState("");
+  const [firstname, seFirstname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section className="signin-ctn">
+        <h2>Créer son compte</h2>
 
-export default Page
+        <form action="">
+          <label htmlFor="email">Nom *</label>
+          <input
+            required
+            placeholder="Votre Nom"
+            type="text"
+            name="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+          <label htmlFor="firstname">Prénom *</label>
+          <input
+            required
+            placeholder="Votre prénom"
+            type="text"
+            name="firstname"
+            value={firstname}
+            onChange={(e) => seFirstname(e.target.value)}
+          />
+          <label htmlFor="email">Email *</label>
+          <input
+            required
+            placeholder="Votre email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="password">Mot de passe *</label>
+          <input
+            required
+            placeholder="Votre mot de passe"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <p className="CGVUs-warning">En validant l`&apos;`inscrption vous acceptez <Link href="/cgvu">les CGVUs, la politique de confidentialité et la gestion des données.</Link></p>
+          <button className="signup-btn" type="submit">Enregistrement</button>
+        </form>
+
+        <p className="signin_to_up">Vous avez déjà un compte ?</p>
+        <p className="signin_to_up link">
+          <Link href="/connexion">Connectez-vous.</Link>
+        </p>
+      </section>
+    </>
+  );
+}
